61ae632573ad7dda0559a3c4e27cc909c55fb95a
Michael Martinez <66656582+lonelydomino@users.noreply.github.com>
2022-06-07 04:23:17 +0000

    update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ age = int(input("What is your current age?"))
 years_remaining = 90 - age
 days = years_remaining * 365
 months = years_remaining * 12
-weeks = years_remaining * 56
+weeks = years_remaining * 52
 
 print(f"You have {days} days, {weeks} weeks and {months} months left. ")
 
